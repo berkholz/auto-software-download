34ef6a4a1a03bd248dbab689724d9206f7da4e4c
Marcel Berkholz <code@berkholz.org>
2017-11-12 01:03:59 +0100

    add checksum check for putty download

--- download_putty.sh
+++ download_putty.sh
@@ -4,24 +4,30 @@ OVERVIEWPAGE_URL="https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html
 
 CURL_COMMAND=/usr/bin/curl
 CURL_OPTIONS=" -L -s "
+SHA256_COMMAND=/usr/bin/sha256sum
 DOWNLOAD_URL=""
 
 function getUrlForRelease {
 	OVERVIEWPAGE=$( $CURL_COMMAND $CURL_OPTIONS ${OVERVIEWPAGE_URL} )
-
 	VERSION_OF_RELEASE=$( echo $OVERVIEWPAGE |tr '>' '\n' |grep -i TITLE|grep -i release|cut -f2 -d'('|cut -f1 -d')')
-#grep "Download PuTTY:" |cut -f2 -d ':' |cut -f1 -d'<'|tr -d ' ' )
-	echo $VERSION_OF_RELEASE
-
 	DOWNLOAD_URL="https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-${VERSION_OF_RELEASE}-installer.msi"
-	echo $DOWNLOAD_URL
 }
 
 function downloadUrl {
 	$CURL_COMMAND $CURL_OPTIONS -O $DOWNLOAD_URL
 }
 
+function verifyDownload {
+	CHECKSUM_FILE_URL="https://the.earth.li/~sgtatham/putty/latest/sha256sums"
+
+	CHECKSUM=$($CURL_COMMAND $CURL_OPTIONS $CHECKSUM_FILE_URL |grep "putty-64bit-${VERSION_OF_RELEASE}-installer.msi"| sed -e "s#w64/##" > "putty-64bit-${VERSION_OF_RELEASE}-installer.msi.sha256")
+	echo -en "Checking sha256 sum: "
+	$SHA256_COMMAND -c "putty-64bit-${VERSION_OF_RELEASE}-installer.msi.sha256"
+}
+
 ############################
 getUrlForRelease
 
 downloadUrl
+
+verifyDownload
